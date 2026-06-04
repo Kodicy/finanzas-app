4dd9748f4265493f5ab85441324ee5491977327b
Kodi <venta@MacBook-del-Hoddies.local>
2026-06-04 03:20:52 -0600

    Fix brace error + swipe delete

--- src/App.jsx
+++ src/App.jsx
@@ -907,6 +907,7 @@ export default function App() {
       };
     });
   };
+  const agregarCuenta = (cuenta) => {
     setData(d=>({...d,cuentas:[...d.cuentas,cuenta]}));
     setShowAddCuenta(false);
   };
